198999776a7e86740f2cdeeb5b5fff028fe5f4f0
kaique.rocha <kaique.rocha@tivit.com.br>
2016-11-07 22:10:43 -0200

    teste Push

--- app/src/main/java/com/tivit/inventariodmt/dataconsistency/sync/SyncAdapter.java
+++ app/src/main/java/com/tivit/inventariodmt/dataconsistency/sync/SyncAdapter.java
@@ -271,7 +271,7 @@ public class SyncAdapter extends AbstractThreadedSyncAdapter {
             String estado = response.getString(Constantes.ESTADO);
             // Obtener mensaje
             //String mensaje = response.getString(Constantes.MENSAJE);
-            // Obtener identificador del nuevo registro creado en el servido
+            // Obtener identificador del nuevo registro creado en el servidor
             String idRemota = response.getString(Constantes.ID_GASTO);
 
             switch (estado) {
